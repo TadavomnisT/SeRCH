0c529c4ea56d2e5a5ec57a723d95f159b03f4407
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2022-05-12 13:19:23 +0430

    Developing initial stuff
     Developing initial stuff : 
    Posting has been checked successfully .
    
    Starting to code, for choosing a Public-key cryptography .

--- development_test_of_serch/test.php
+++ development_test_of_serch/test.php
@@ -5,16 +5,15 @@ require_once "Connection_handler.php";
 
 $ch = new Connection_handler( "http://7f16bce141c6afad5fe2266b71e280f0.gigfa.com" );
 
-
-var_dump( 
+var_dump(
+    // openssl_public_encrypt() 
     $ch->getBaseUrlContents(
         [
-            "post_test" => "this is post test",
-            "post_test1" => "this is post test"
+            "post_paramter_1" => hash( "sha256" , rand( 0 , 9999999999 ) ),
+            "post_paramter_2" => hash( "sha256" , rand( 0 , 9999999999 ) )
         ]
     )
  );
-
 
 
 ?>
