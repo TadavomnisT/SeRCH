dc2a3a8398d81b9597a894ce5aea3b18074348e8
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2022-06-05 20:09:18 +0430

    Testing pkey openssl functions
    Testing pkey openssl functions, some functions are a bit...

--- development_test_of_serch/openssl_public_key_crypto_example.php
+++ development_test_of_serch/openssl_public_key_crypto_example.php
@@ -1,46 +1,84 @@
 <?php
 
-// Make a new private key
+//Script by TadavomnisT
+
+// *Note*
+// Check out this line:
+// 
+//            $private_key = openssl_pkey_new();
+// 
+// Although we named the object as "$private_key" , but,
+// It is not just private key!
+// In truth, there's a "key-pair" of both public amd private key
+// generated in that object, but if you pass the object to the functions,
+// it'll behave as "private-key" .
+// In order to fetch "private-key" as an object , you need to either 
+// * (APPROACH#1) Do as what follows ( get detail and fetch)
+// or
+// * (APPROACH#2)Store in a file in PEM format then fetch ( explained in last part of this code )
+
+
+// (APPROACH#1) :
+
 $private_key = openssl_pkey_new();
+$public_key_pem = openssl_pkey_get_details($private_key)['key'];
+$public_key = openssl_pkey_get_public($public_key_pem);
 
+$plain_data = "This is a secret message";
 
-// Save Private Key
-openssl_pkey_export_to_file($private_key, "privatekey.pem");
+// Test #1
+// Encrypt with private-key
+// Only dectryptable with public-key------------------------------------------
 
+$cipher_data = "";
+$deciphered_data = "";
 
+$state_1 = openssl_private_encrypt($plain_data, $cipher_data, $private_key);
+$state_2 = openssl_public_decrypt($cipher_data, $deciphered_data, $public_key);
 
-// var_dump( 
-//     openssl_pkey_get_details($private_key
-// );
-
-
-
-
-//Save Public Key
-$dn = array(
-    // "countryName" => "IN",
-    // "stateOrProvinceName" => "Karnataka",
-    // "localityName" => "test1",
-    // "organizationName" => "test2",
-    // "organizationalUnitName" => "test3",
-    // "commonName" => "www.test.com",
-    // "emailAddress" => "xyz@test.com"
+var_dump(
+    $state_1,
+    $state_2,
+    $cipher_data,
+    $deciphered_data
 );
+
+// Test #2
+// Encrypt with public-key
+// Only dectryptable with private-key------------------------------------------
+
+$cipher_data = "";
+$deciphered_data = "";
+
+$state_1 = openssl_public_encrypt($plain_data, $cipher_data, $public_key);
+$state_2 = openssl_private_decrypt($cipher_data, $deciphered_data, $private_key);
+
+var_dump(
+    $state_1,
+    $state_2,
+    $cipher_data,
+    $deciphered_data
+);
+
+
+
+// ------------------------------------------------------------------------------------
+
+// (APPROACH#2) :
+
+$private_key = openssl_pkey_new();
+openssl_pkey_export_to_file($private_key, "privatekey.pem");
+ 
+//Save Public Key
+$dn = array();
 $cert = openssl_csr_new($dn, $private_key);
 $cert = openssl_csr_sign($cert, null, $private_key, 365);
 openssl_x509_export_to_file($cert, "publickey.pem");
-
-
-// To encrypt data
-$data = "This is a secret message!";
-$isvalid = openssl_public_encrypt ($data, $crypted , file_get_contents("publickey.pem"),OPENSSL_PKCS1_PADDING);	
-echo "Data encryption : ".$crypted;
-echo ">br/<>br/<";
-
-if ($isvalid) {	
-    openssl_private_decrypt ($crypted, $decrypted , file_get_contents("privatekey.pem"),OPENSSL_PKCS1_PADDING);	
-    echo "Data decryption : ".$decrypted;
-}
+ 
+ 
+$data = "This is a secret message";
+$isvalid = openssl_public_encrypt ($data, $encrypted , file_get_contents("publickey.pem"),OPENSSL_PKCS1_PADDING);	
+echo "Data encryption : ".$encrypted;
 
 
 ?>
