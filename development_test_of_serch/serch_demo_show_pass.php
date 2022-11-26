96de54e80981f137f32809aea4a07a36213d1ca1
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2022-11-26 23:11:52 +0330

    Demo version 1 : Beta Tetsing

--- development_test_of_serch/serch_demo_show_pass.php
+++ development_test_of_serch/serch_demo_show_pass.php
@@ -132,29 +132,8 @@ class SeRCH
         }
     }
     function prompt_silent($prompt = "Enter Password:") {
-        if (preg_match('/^win/i', PHP_OS)) {
-          $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
-          file_put_contents(
-            $vbscript, 'wscript.echo(InputBox("'
-            . addslashes($prompt)
-            . '", "", "password here"))');
-          $command = "cscript //nologo " . escapeshellarg($vbscript);
-          $password = rtrim(shell_exec($command));
-          unlink($vbscript);
-          return $password;
-        } else {
-          $command = "/usr/bin/env bash -c 'echo OK'";
-          if (rtrim(shell_exec($command)) !== 'OK') {
-            trigger_error("Can't invoke bash");
-            return;
-          }
-          $command = "/usr/bin/env bash -c 'read -s -p \""
-            . addslashes($prompt)
-            . "\" mypassword && echo \$mypassword'";
-          $password = rtrim(shell_exec($command));
-          echo "\n";
-          return $password;
-        }
+        echo $prompt;
+        return trim(fread( STDIN , 4096 )); 
     }
     function print_help()
     {
